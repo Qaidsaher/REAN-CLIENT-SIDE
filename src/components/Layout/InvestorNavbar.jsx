2042dd15024406d2938413655b5dae5e470b50d5
SaherQaid <saherqaid2020@gmail.com>
2025-04-27 01:55:58 +0300

    fixing the name of website

--- src/components/Layout/InvestorNavbar.jsx
+++ src/components/Layout/InvestorNavbar.jsx
@@ -37,7 +37,7 @@ const InvestorNavbar = () => {
           {/* ✅ Logo */}
           <div className="flex items-center">
             <NavLink to="/" className="text-2xl font-bold text-indigo-600">
-              Ream
+            REAN
             </NavLink>
           </div>
 
